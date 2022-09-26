fbdc8e79f08a37d5c01202a35dae978a18575d17
Yeison Cardona <yencardonaal@unal.edu.co>
2022-09-26 16:17:28 -0500

    First commit

--- index.py
+++ index.py
@@ -5,43 +5,43 @@ from radiant.server import RadiantAPI, RadiantServer, pyscript, delay
 from browser import document, html, timer
 from bootstrap.btn import Button
 import logging
-from radiant.utils import WebSocket
+# from radiant.utils import WebSocket
 
 
-########################################################################
-class HackLights(WebSocket):
-    """"""
+# ########################################################################
+# class HackLights(WebSocket):
+    # """"""
 
-    DATA = {
-        # "address": 5894,
-        "datatype": 9,
-        # "value": "600",
-        "type": "text",
-        "update": True,
-        "action": "write",
-    }
+    # DATA = {
+        # # "address": 5894,
+        # "datatype": 9,
+        # # "value": "600",
+        # "type": "text",
+        # "update": True,
+        # "action": "write",
+    # }
 
-    ROOM = {
-        'EEG': 7942,
-        'BACK': 7942,
-        'FRONT': 7942,
-    }
+    # ROOM = {
+        # 'EEG': 7942,
+        # 'BACK': 7942,
+        # 'FRONT': 7942,
+    # }
 
-    # ----------------------------------------------------------------------
-    def on(self, room):
-        """"""
-        data = self.DATA.copy()
-        data['addres'] = self.ROOM[room]
-        data['value'] = '600'
-        self.send(data)
+    # # ----------------------------------------------------------------------
+    # def on(self, room):
+        # """"""
+        # data = self.DATA.copy()
+        # data['addres'] = self.ROOM[room]
+        # data['value'] = '600'
+        # self.send(data)
 
-    # ----------------------------------------------------------------------
-    def off(self, room):
-        """"""
-        data = self.DATA.copy()
-        data['addres'] = self.ROOM[room]
-        data['value'] = '0'
-        self.send(data)
+    # # ----------------------------------------------------------------------
+    # def off(self, room):
+        # """"""
+        # data = self.DATA.copy()
+        # data['addres'] = self.ROOM[room]
+        # data['value'] = '0'
+        # self.send(data)
 
 
 ########################################################################
@@ -51,10 +51,10 @@ class UNLights(RadiantAPI):
     def __init__(self, *args, **kwargs):
         """"""
         super().__init__(*args, **kwargs)
-        self.ws = HackLights('ws://172.20.176.40/scada-vis/objects/ws')
+        # self.ws = HackLights('ws://172.20.176.40/scada-vis/objects/ws')
 
-        document.select_one('body') <= Button('On', on_click=lambda evt: self.ws.on('EEG'))
-        document.select_one('body') <= Button('Off', on_click=lambda evt: self.ws.off('EEG'))
+        # document.select_one('body') <= Button('On', on_click=lambda evt: self.ws.on('EEG'))
+        # document.select_one('body') <= Button('Off', on_click=lambda evt: self.ws.off('EEG'))
 
 
 if __name__ == '__main__':
